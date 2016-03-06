338f1c71c611cb7c61a2650a3392b3990eefc439
Tim Fulmer <me@timfulmer.com>
2016-03-05 21:43:47 -0800

    End to end MVP

--- app/config/express.js
+++ app/config/express.js
@@ -5,6 +5,7 @@
 
 var express=require('express'),
   bodyParser = require('body-parser'),
+  cors=require('cors'),
   Promise=require('bluebird'),
   routes=require('./routes');
 
@@ -17,11 +18,7 @@ function initialize(options){
       extended: true
     }));
     app.use(bodyParser.json());
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-    });
+    app.use(cors());
     options.express=app;
     routes(options);
     var server=app
--- app/controllers/throws.js
+++ app/controllers/throws.js
@@ -9,7 +9,7 @@ var mongoose=require('mongoose'),
 
 function *createThrow(req,res){
   if(!req.body.playerName || !req.body.playerThrow){
-    return res.send(400,'No player throw information in request body.');
+    return res.status(400).send('No player throw information in request body.');
   }
   var thro=yield new Throw(req.body).save();
   return res.json(thro);
@@ -20,16 +20,24 @@ function *openThrows(req,res){
 }
 function *playThrow(req,res){
   if(!req.params.throwId){
-    return res.send(400,'No throwId in request parameters.');
+    return res.status(400).send('No throwId in request parameters.');
   }
   if(!req.body.opponentName || !req.body.opponentThrow){
-    return res.send(400,'No opponent throw information in request body');
+    return res.status(400).send('Missing opponent information in request body');
   }
   var thro=yield Throw.findOne({_id:new ObjectId(req.params.throwId)});
+  thro.opponentName=req.body.opponentName;
+  thro.opponentThrow=req.body.opponentThrow;
+  thro.renderJudgement();
+  thro=yield thro.save();
   return res.json(thro);
 }
 function *closedThrows(req,res){
-  var throws=yield Throw.find({opponentThrow:{$exists:true},opponentName:{$exists:true}});
+  var throws=yield Throw
+    .find({opponentThrow:{$exists:true},opponentName:{$exists:true}});
+  throws=throws.sort(function(a,b){
+    return b.created.getTime()-a.created.getTime();
+  });
   return res.json(throws);
 }
 
